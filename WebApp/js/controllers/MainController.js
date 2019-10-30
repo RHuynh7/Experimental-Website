79cb961adddd999d9e0f6403a7964c149926854c
RHuynh7 <54679164+RHuynh7@users.noreply.github.com>
2019-10-30 09:24:54 -0400

    main controller added

---
+++ WebApp/js/controllers/MainController.js
@@ -0,0 +1,20 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'This Month\'s Bestsellers';
+  $scope.promo = 'The most popular books this month.';
+  $scope.products = [
+  	{
+    	name: 'Stargazer',
+    	price: 19,
+    	pubdate: new Date('2014', '03', '08'),
+    	cover: 'img/stargazer.png',
+    	likes: 0,
+    	dislikes: 0
+  	}
+  ];
+  $scope.plusOne = function(index) {
+  	$scope.products[index].likes += 1;
+	};
+	$scope.minusOne = function(index) {
+  	$scope.products[index].dislikes += 1;
+	};
+}]);
