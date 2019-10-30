332a4930eabf82f4b077e611c003101816edbbf8
Student <jsitbon@qcc.cuny.edu>
2019-10-30 11:31:04 -0400

    I FIGURED ANGULAR OUT SORT OF

--- WebApp/js/controllers/MainController.js
+++ WebApp/js/controllers/MainController.js
@@ -1,47 +1,79 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'Portfolio';
-  $scope.promo = 'My works';
+  $scope.title = 'Artwork';
+  $scope.promo = 'Both digital and traditional art';
   $scope.products = [
   	{
     	name: 'Stargazer',
-    	price: 19,
-    	pubdate: new Date('2014', '03', '08'),
-    	cover: 'img/stargazer.png',
-    	likes: 0,
-    	dislikes: 0
+    	cover: 'img/stargazer.png'
   	},
     {
-    	name: 'Stargazer',
-    	price: 19,
-    	pubdate: new Date('2014', '03', '08'),
-    	cover: 'img/stargazer.png',
-    	likes: 0,
-    	dislikes: 0
+      name: 'Stargazer',
+      cover: 'img/Stargazer-feather-hoop.png'
+    },
+    {
+      name: 'Stargazer',
+      cover: 'img/stargazer-one.png'
+    },
+    {
+      name: 'Stargazer',
+      cover: 'img/stargazer-original.png'
+    },
+    {
+      name: 'LOONA Galaxy',
+      cover: 'img/loona-black.png'
+    },
+    {
+      name: 'Seudo Productions',
+      cover: 'img/seudo-productions.jpg'
+    },
+    {
+      name: 'Untitled',
+      cover: 'img/mash.jpg'
+    },
+    {
+    	name: 'Gunfire',
+    	cover: 'img/political.jpg'
   	},
     {
-    	name: 'Stargazer',
-    	price: 19,
-    	pubdate: new Date('2014', '03', '08'),
-    	cover: 'img/stargazer.png',
-    	likes: 0,
-    	dislikes: 0
+    	name: 'Visionaire Poster',
+    	cover: 'img/Visionaire.jpg'
   	},
     {
-    	name: 'Stargazer',
-    	price: 19,
-    	pubdate: new Date('2014', '03', '08'),
-    	cover: 'img/stargazer.png',
-    	likes: 0,
-    	dislikes: 0
+    	name: 'Wolf Kings Poster',
+    	cover: 'img/Wolf-Kings.jpg'
   	},
     {
-    	name: 'Stargazer',
-    	price: 19,
-    	pubdate: new Date('2014', '03', '08'),
-    	cover: 'img/stargazer.png',
-    	likes: 0,
-    	dislikes: 0
+      name: 'Blind Owl',
+      cover: 'img/blind-owl.jpg'
+    },
+    {
+      name: 'Moon',
+      cover: 'img/shinee-moon.jpg'
+    },
+    {
+      name: 'Butterfly',
+      cover: 'img/butterfly.png'
+    },
+    {
+      name: 'EXO Studios',
+      cover: 'img/exo-studios.jpg'
+    },
+    {
+    	name: 'Wiccan Sweets Shoppe',
+    	cover: 'img/wiccan-sweets-shoppe.jpg'
   	},
+    {
+    	name: 'Sunrise Airlines',
+    	cover: 'img/sunrise-airlines.jpg'
+  	},
+    {
+    	name: 'Medieval Poker Club',
+    	cover: 'img/medieval-poker-club.jpg'
+  	},
+    {
+      name: 'Tweet Inc.',
+      cover: 'img/tweet-inc.jpg'
+    },
   ];
   $scope.plusOne = function(index) {
   	$scope.products[index].likes += 1;
